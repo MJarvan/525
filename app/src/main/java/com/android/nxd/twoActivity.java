d1510cfe25395b646840870f989900ed13babfdc
mj <mj.630313091@qq.com>
2016-03-29 18:41:20 +0800

    add

---
+++ app/src/main/java/com/android/nxd/twoActivity.java
@@ -0,0 +1,15 @@
+package com.android.nxd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by admin on 2016/3/24.
+ */
+public class twoActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two);
+    }
+}
